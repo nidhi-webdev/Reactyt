fc2d28bbd4148890b2b48f4031f6590e4d9fb042
nidhi <kumarinidhi0594@gmail.com>
2025-09-08 13:03:55 +0200

    Decreament Increment Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const name = "Nidhi"
-  const functionCall = () => {
-    console.log("hello")
-  }
-  return <div> I am {name} 
-       <button onClick={functionCall}> Click me </button>
-  </div>
+const App = () => {
+const [num, setNum] = useState(0)
+
+
+  return (
+<div>
+  <h1> Number is {num} </h1>
+<button onClick={() => setNum(num + 10)}> Increment </button>
+<button onClick={() => setNum(num - 10)}> Decrement </button>
+</div>
+  )
 }
 
 export default App
