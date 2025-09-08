bdf3a84eec01d9c84d1898a71f8c522112777b3f
nidhi <kumarinidhi0594@gmail.com>
2025-09-08 15:21:50 +0200

    Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,16 @@ import './style.css'
 
 
 const App = () => {
-  const [username, setUserName] = useState('')
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    console.log(username)
-    setUserName('')
-  }
+ 
   return (
-    <div>
-      <form onSubmit={(e) => {
-        submitHandler(e)
-      }}>
-        <input
-          value={username}
-          onChange={(e) => {
-            setUserName(e.target.value)
-          }}
-          type='text' placeholder='write something' className='px-4 py-4 border border-black m-4' />
-        <button className='bg-emerald-300'> submit </button>
-      </form>
-    </div>
+    <>
+    <h1>Hello   </h1>
+     <h1>Hello2  </h1>
+      <h1>Hello3  </h1>
+  
+      
+    </>
   )
 }
 
