086fb6dc52b7bcb5f360e56b75707a41371c047e
nidhi <kumarinidhi0594@gmail.com>
2025-09-08 15:18:04 +0200

    Form Handle

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 
 const App = () => {
- const submitHandler = (e) => {
-  e.preventDefault()
-  console.log("hello")
- }
+  const [username, setUserName] = useState('')
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(username)
+    setUserName('')
+  }
   return (
     <div>
-    <form onSubmit={(e) => {
-      submitHandler(e)
-    }}>
-      <input type='text' placeholder='write something' className='px-4 py-4 border border-black m-4'/>
-      <button className='bg-emerald-300'> submit </button>
-    </form>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input
+          value={username}
+          onChange={(e) => {
+            setUserName(e.target.value)
+          }}
+          type='text' placeholder='write something' className='px-4 py-4 border border-black m-4' />
+        <button className='bg-emerald-300'> submit </button>
+      </form>
     </div>
   )
 }
